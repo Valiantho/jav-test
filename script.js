d184a884daa1869734010f991fc11530eb8f1238
valiantho <bigbraint328@gmail.com>
2023-05-11 12:02:50 +0100

    Created a funtion that takes 2 parameters playerSelection and computerSelection and returns a ties if the selectionare same or winner according to the rule of the game

---
+++ script.js
@@ -0,0 +1,33 @@
+/**Your game is going to play against the computer, so begin with a function 
+called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ 
+or ‘Scissors’. We’ll use this function in the game to make the computer’s 
+play. Tip: use the console to make sure this is returning the expected 
+output before moving to the next step!*/
+
+function getComputerChoice() {
+    let Chioce = ['Rock', 'Paper', 'Scissors'];
+    let cal = Math.floor(Math.random() * Chioce.length);
+    let computerChoice = Chioce[cal];
+    
+    console.log(computerChoice);
+}
+
+/* Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - the playerSelection and 
+computerSelection - and then return a string that declares the winner 
+of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so 
+users can input rock, ROCK, RocK or any other variation).*/
+
+function gamePlay(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        console.log('its a tie');
+    }
+
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')){
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        } 
+}
+gamePlay('rock', 'scissors')
