4422bd1fe8ca558853b02c2a865116128d3d96f8
valiantho <bigbraint328@gmail.com>
2023-05-13 19:39:53 +0100

    added a loop to play 5 rounds

--- script.js
+++ script.js
@@ -22,21 +22,54 @@ users can input rock, ROCK, RocK or any other variation).*/
 function gamePlay(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection){
-        console.log('its a tie');
+        return 'its a tie';
     }
 
    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')){
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
         } else{
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
         }};
 
-        let computerChoice = getComputerChoice();
-        let playerChoice = prompt('Write rock, paper or scissors', 'input here');
+       
 
-gamePlay(playerChoice, computerChoice)
+      /*  Write a NEW function called game(). Use the previous function inside of this 
+        one to play a 5 round game that keeps score and reports a winner or loser at the end.
+        You have not officially learned how to “loop” over code to repeat function calls… if 
+        you already know about loops from somewhere else (or if you feel like doing some more learning) 
+        feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. 
+        Loops are covered in the next lesson.*/
+
+        function game(){
+        for (i=0; i < 5; i++){
+           
+            let computerChoice = getComputerChoice();
+            let playerChoice = prompt('Write rock, paper or scissors', 'input here');
+            let result = gamePlay(playerChoice, computerChoice);
+            let computerScore = 0;
+            let playerScore = 0;
+
+            if (result === `You Win! ${playerChoice} beats ${computerChoice}`){
+                playerScore++;
+                console.log(`Round ${i+1}: Player Wins!`);
+            }
+            else if (result === `You Lose! ${computerChoice} beats ${playerChoice}`){
+
+                console.log(`Round ${i+1}: Computer Wins!`)
+            } 
+            else if (result === 'its a tie'){
+                console.log(`Round ${i+1}: 'its a tie`)
+            }
+    
+            }
+    
+            }
+            game()
+
+        
+
 
 /*Important note: you want to return the results of this function call, not console.log() them. 
 You’re going to use what you return later on, so let’s test this function by using console.log 
